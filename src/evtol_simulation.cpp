a92493c3f9a0e64e9fb264870ca002a8c4d2a961
Gabriel Staples <ercaguy@gmail.com>
2023-03-09 11:35:38 -0700

    wip: begin constructing classes

--- src/evtol_simulation.cpp
+++ src/evtol_simulation.cpp
@@ -4,15 +4,57 @@ Main simulation file
 
 // C++ includes
 #include <iostream>
+#include <vector>
 
 // C includes
-#include <cstdint>  // For `uint8_t`, `int8_t`, etc.
-#include <cstdio>   // For `printf()`
+//////
+// #include <cstdint>  // For `uint8_t`, `int8_t`, etc.
+// #include <cstdio>   // For `printf()`
+
+class Company
+{
+public:
+
+}
+
+class Vehicle
+{
+public:
+    constexpr char* company_name;
+    constexpr double cruise_speed_mph;
+    constexpr double battery_capacity_kwh;
+    constexpr double time_to_charge_hrs;
+    constexpr double energy_in_cruise_kwh_per_mile;
+    constexpr uint16_t max_passenger_cnt;
+    /// mean probability of faults per hour
+    constexpr double prob_fault_per_hr;
+
+    // constructor
+    Vehicle(double )
+
+    {}
+}
+
+class Simulation
+{
+public:
+    void add_aircraft(Vehicle vehicle)
+    {
+        _aircraft.push_back(vehicle);
+    }
+
+private:
+    std::vector<Vehicle> _aircraft;
+}
 
 int main()
 {
-    printf("Hello ");
-    std::cout << "world!\n\n";
+    std::cout << "Running simulation\n\n";
+
+    Simulation simulation;
+
+    // Populate all vehicle
+    simulation.add_aircraft({"Alpha", 120, 320, 0.6, 1.6, 4, 0.25})
 
     return 0;
 }
