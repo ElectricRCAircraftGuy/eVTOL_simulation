fe0d5396e9df9f96fcfbc62febbf2159385edc60
Gabriel Staples <ercaguy@gmail.com>
2023-03-10 11:35:09 -0700

    wip: continue figuring out program architecture
    	modified:   src/evtol_simulation.cpp
    	new file:   src/evtol_simulation_unittest.cpp

--- src/evtol_simulation.cpp
+++ src/evtol_simulation.cpp
@@ -2,13 +2,23 @@
 Main simulation file
 
 Todo: break apart into multiple modules, each with a header and source file.
+
+Assumptions:
+- faults don't make the vehicle stop flying; it keeps flying; you just count the faults
+- when the plane runs out of fuel it is instantly at the charger without adding distance
+- only charge when the battery is empty (at 0% State of Charge (SoC))
+
 */
 
 // C++ includes
 #include <iostream>
 #include <random>
 #include <string>
+#include <unordered_set>
 #include <vector>
+
+namespace evtol_simulation
+{
 
 // C includes
 //////
@@ -18,10 +28,22 @@ Todo: break apart into multiple modules, each with a header and source file.
 // TODO: read these as input arguments to `main()`.
 constexpr uint32_t NUM_VEHICLES = 20;
 constexpr uint32_t NUM_CHARGERS = 3;
-constexpr double SIMULATION_TIME_HRS = 3.0;
+constexpr double SIMULATION_DURATION_HRS = 3.0;
+
+struct Vehicle_type_stats
+{
+    ////////
+    // avg_flight_time_per_flight_hrs;
+    // avg_dist_per_flight_miles;
+    // avg_
+    uint32_t total_num_flights
+    /////// add the rest
+};
 
 struct Vehicle_type
 {
+    // primary values
+
     /// vehicle name (same as "company name" in this case, since each company only has one vehicle
     /// type)
     const std::string name;
@@ -32,6 +54,12 @@ struct Vehicle_type
     const uint16_t passenger_cnt;
     /// mean probability of faults per hour
     const double prob_fault_per_hr;
+
+    // derived values
+
+    //////
+    const double cruise_power_kw;
+    const double range_miles;
 
     // constructor
     Vehicle_type(
@@ -66,28 +94,48 @@ struct Vehicle_type
     }
 };
 
+enum class Vehicle_state
+{
+    FLYING = 0,
+    WAITING_FOR_CHARGER,
+    CHARGING,
+};
+
 struct Vehicle_stats
 {
-    // // Vehicle stats
-    // // TODO: Move to its own struct
-    // avg_flight_time_per_flight_hrs;
-    // avg_dist_per_flight_miles;
-    // avg_
+    uint32_t num_flights = 0;
+    double flight_time_hrs = 0;
+    double distance_traveled_miles = 0;
 
+    uint32_t num_charge_sessions = 0;
+    double charge_time_hrs = 0;
+
+    uint32_t num_faults = 0;
+
+    Vehicle_state vehicle_state = Vehicle_state::FLYING;
 };
 
 class Vehicle
 {
 public:
     // constructor
-    Vehicle(Vehicle_type vehicle_type)
-        : _vehicle_type{vehicle_type}
+    Vehicle(size_t i_vehicle_type, const std::vector<Vehicle_type>& vehicle_types)
+    : _i_vehicle_type{i_vehicle_type}
     {}
 
-private:
-    Vehicle_type _vehicle_type;
-    Vehicle_stats _vehicle_stats;
+    //////// move to Simulation class?
+    const Vehicle_type& get_vehicle_type()
+    {
+        return _vehicle_types[_i_vehicle_type];
+    }
 
+private:
+    /// for rapid O(1) mapping to the vehicle type
+    const std::vector<Vehicle_type>& _vehicle_types;
+    /// an index to map to the vehicle type
+    const size_t _i_vehicle_type;
+
+    Vehicle_stats _stats;
 };
 
 // class Company
@@ -123,12 +171,20 @@ public:
     {
     }
 
-    // TODO: add error handling to check if a certain type (name) was already added. Do this in
-    // O(1) time by changing the `_vehicle_types` type to an unordered_map instead of a vector.
-    // Update all other parts of the code as necessary for this type change.
-    void add_vehicle_type(Vehicle_type vehicle_type)
+    // returns true if successful and false otherwise
+    // TODO: improve error handling; ex: be returning enums instead
+    bool add_vehicle_type(Vehicle_type vehicle_type)
     {
+        // don't add the same vehicle type more than once
+        if (_vehicle_type_names.count(vehicle_type.name) == 1)
+        {
+            printf("Error: this vehicle type (%s) was already added.\n", vehicle_type.name.c_str());
+            return false;
+        }
+
+        _vehicle_type_names.insert(vehicle_type.name);
         _vehicle_types.push_back(vehicle_type);
+        return true;
     }
 
     void add_random_vehicle() //////// refactor: make private; move the rng stuff to populate_vehicles
@@ -180,6 +236,9 @@ private:
     std::vector<Vehicle> _vehicles;
     uint32_t _num_chargers = 0;
 
+    /// Used to keep track of whether or not a particular vehicle type has already been added
+    std::unordered_set<std::string> _vehicle_type_names;
+
     // For random number generation
 
     std::random_device _random_device;
@@ -191,7 +250,7 @@ int main()
 {
     std::cout << "Running simulation\n\n";
 
-    Simulation simulation;
+    Simulation simulation{NUM_CHARGERS, SIMULATION_DURATION_HRS};//// add this
 
     // Add the various company vehicle types and stats
     // TODO: store and read these from a .yaml file.
@@ -206,7 +265,7 @@ int main()
     simulation.print_vehicle_types();
 
     simulation.set_num_chargers(NUM_CHARGERS);
-    // simulation.set_simulation_time(SIMULATION_TIME_HRS);//////////
+    // simulation.set_simulation_duration(SIMULATION_DURATION_HRS);//////////
 
     // Randomly populate the correct number of vehicles
     simulation.populate_vehicles(NUM_VEHICLES);
@@ -219,4 +278,7 @@ int main()
     return 0;
 }
 
-//////state machine: fly, wait for charger, charge, repeat
+//////add state machine to the vehicle: fly, wait for charger, charge, repeat
+
+
+} // namespace evtol_simulation
