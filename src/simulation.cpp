2f66192458d80d6b5a05993a6cf60855b05728b7
Gabriel Staples <ercaguy@gmail.com>
2023-03-13 16:15:23 -0700

    Run clang-format again after some final tweaks

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -3,11 +3,11 @@
 Simulation::Simulation(
     uint32_t num_chargers,
     double simulation_duration_hrs,
-    double simulation_step_size_hrs) :
-    _num_chargers{num_chargers},
-    _simulation_duration_hrs{simulation_duration_hrs},
-    _simulation_step_size_hrs{simulation_step_size_hrs},
-    _num_chargers_available{_num_chargers}
+    double simulation_step_size_hrs)
+    : _num_chargers{num_chargers},
+      _simulation_duration_hrs{simulation_duration_hrs},
+      _simulation_step_size_hrs{simulation_step_size_hrs},
+      _num_chargers_available{_num_chargers}
 {
 }
 
